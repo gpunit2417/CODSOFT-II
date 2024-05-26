1aba3a87189f42c1e6937f8917ae1df8b799b4fc
Punit Goyal <punitgoyal106@gmail.com>
2024-05-26 12:08:28 +0530

    defines the quiz model

---
+++ server/models/Quiz.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const quizSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  questions: [
+    {
+      question: { type: String, required: true },
+      options: [{ type: String, required: true }],
+      answer: { type: String, required: true },
+    },
+  ],
+});
+
+const Quiz = mongoose.model("Quiz", quizSchema);
+
+module.exports = Quiz;
