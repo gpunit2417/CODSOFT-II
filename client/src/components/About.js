af296701735b02f0c52b58eaed5dcb22d20ad403
Punit Goyal <punitgoyal106@gmail.com>
2024-05-26 12:03:40 +0530

    About Page for the application

---
+++ client/src/components/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+    <div style={{background: 'linear-gradient(to right, #2193b0, #6dd5ed)', height: '95vh'}}>
+      <h1 style={{ margin: "60px auto 30px auto", color: 'white', padding: '20px 0 0 0' }}>About Quiz Game</h1>
+      <p className='card' style={{fontSize: '150%', textAlign: 'left', margin: '0 20px', padding: '20px'}}>
+        Our quiz games put the fun into learning. Your knowledge will be tested
+        regarding a variety of subjects, including geography, cultures, and
+        cartoons. Well-traveled players will love showcasing their map skills in
+        every continent. Answer questions about international borders and
+        capitals to score points and advance. If you get stuck, certain games
+        allow hints and retries. The objective is always to earn a perfect
+        score. Play again and again to achieve a 100% rating! <br />
+        <br />
+        Quizzes and tests aren't just for education and learning. Our quiz games
+        also include fun, whimsical, and unique questions. Fans of tricky
+        questions and riddles can enjoy Brain Test: Tricky Puzzles. If humor and
+        laughter is your thing, then you will enjoy The Impossible Quiz. You can
+        also test your geography skills with games like Flags Maniac, and show
+        everyone how good you are at numbers with Math Trivia Lite. With our
+        quiz games, you can improve your knowledge, and showcase your
+        intelligence to set the class record!
+      </p>
+      </div>
+      <div style={{background: 'linear-gradient(to right, #2193b0, #6dd5ed)', height: '85vh'}}>
+      <h1 style={{margin: '50px 0', color: 'white', padding: '20px 0 0 0'}}>What we provide?</h1>
+      <p className='card' style={{fontSize: '150%', textAlign: 'left', margin: '0 20px 0 20px', padding: '20px'}}>
+        Our Quiz Games not only provide opportunity for just learning purpose, but also provides an opportunity for the users to create their own Quiz Games. They are free to create the Quiz Games of their own interest.
+        Also, our Quiz Games provide an ample opportunity for almost all the individuals of all ages to play the Quiz Games based on their interest.
+        <br/><br/>
+        Different domains for the interest like:<br/>
+        1. Programming Quiz<br/>
+        2. Quiz For Kids<br/>
+        3. General Knowledge Quiz
+      </p>
+    </div>
+    </>
+  );
+}
